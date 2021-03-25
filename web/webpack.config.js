afa22c0cfa38dc9b8ba09ed120fca5521d0c0a06
Fikry Fahrezy Ramadhan <ffahrezi69@gmail.com>
2021-03-25 22:14:44 +0700

    move development to local and move from parcel to webpack

---
+++ web/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './src',
+        hot: true,
+    },
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+};
