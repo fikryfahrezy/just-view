744402402337c84bf51a1c470193664b94296e83
Fikry Fahrezy Ramadhan <ffahrezi69@gmail.com>
2021-04-02 22:52:49 +0700

    creating masonry using desandro/masonry cdn

--- web/src/script.js
+++ web/src/script.js
@@ -1,54 +1,30 @@
-import imagesLoaded from 'imagesloaded';
-
-console.log('hi');
-
-const main = document.getElementById('main');
-
-// https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
-function resizeGridItem(item) {
-    const rowHeight = parseInt(
-        window.getComputedStyle(main).getPropertyValue('grid-auto-rows'),
-        10
-    );
-    const rowGap = parseInt(
-        window.getComputedStyle(main).getPropertyValue('grid-row-gap'),
-        10
-    );
-    const rowSpan = Math.ceil(
-        (item.querySelector('.content').getBoundingClientRect().height +
-            rowGap) /
-            (rowHeight + rowGap)
-    );
-    item.style.gridRowEnd = 'span ' + rowSpan;
-}
-
-function resizeInstance(instance) {
-    const item = instance.elements[0];
-    resizeGridItem(item);
-}
+const grid = document.getElementById('grid');
 
 function itemComponent(imageUrl) {
     const item = document.createElement('div');
-    item.classList.add('item');
-    const content = document.createElement('div');
-    content.classList.add('content');
-    item.appendChild(content);
+    item.classList.add('grid-item');
     const img = document.createElement('img');
-    img.classList.add('photothumb');
     img.src = imageUrl;
     img.alt = 'View Image';
-    content.appendChild(img);
+    item.appendChild(img);
     return item;
 }
 
 fetch('https://picsum.photos/v2/list?limit=100')
     .then((res) => res.json())
-    .then((res) =>
+    .then((res) => {
         res.forEach((item) => {
             const itemComp = itemComponent(item.download_url);
-            main.appendChild(itemComp);
-            resizeGridItem(itemComp);
-            imagesLoaded(itemComp, resizeInstance);
-        })
-    )
+            grid.appendChild(itemComp);
+        });
+    })
+    .then(() => {
+        imagesLoaded(grid, () => {
+            new Masonry(main, {
+                itemSelector: '.grid-item',
+                columnWidth: '.grid-sizer',
+                percentPosition: true,
+            });
+        });
+    })
     .catch((err) => console.log(err));
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     devServer: {
         contentBase: './src',
         hot: true,
+        host: '0.0.0.0',
     },
     entry: './src/index.js',
     module: {
