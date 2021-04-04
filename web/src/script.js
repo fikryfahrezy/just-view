07a3e317706ba24097bdf12cc9ec735512316297
Fikry Fahrezy Ramadhan <ffahrezi69@gmail.com>
2021-04-04 22:22:11 +0700

    replace masonry-layout with isotope-layout and configure responsiveness

--- web/src/script.js
+++ web/src/script.js
@@ -1,43 +1,95 @@
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
+import Isotope from 'isotope-layout';
 import 'lazysizes';
 import 'lazysizes/plugins/blur-up/ls.blur-up';
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
-const snapContainer = document.getElementById('snap-container');
-const scrollButton = document.querySelector('.scroll-button');
-const main = document.querySelector('main');
-const grid = document.getElementById('grid');
 const blurImg =
     'https://drive.google.com/uc?export=view&id=1L9QleiNcxQMWdS7M5urFTpz85RTtpJMy';
-const masonry = new Masonry(grid, {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true,
-});
+const grid = document.querySelector('.grid');
+const scrollButton = document.querySelector('.scroll-button');
+let modal = null;
 
 const itemComponent = function itemComponent(image) {
     const { download_url, height, width, author } = image;
     const item = document.createElement('div');
-    item.classList.add('grid-item', 'wrapper');
-    const mediabox = document.createElement('div');
-    mediabox.classList.add('mediabox');
-    item.appendChild(mediabox);
+    item.classList.add('grid-item');
     const img = document.createElement('img');
-    img.classList.add('mediabox-img', 'lazyload');
-    img.dataset.src = download_url;
-    img.dataset.lowsrc = blurImg;
+    item.appendChild(img);
+    img.classList.add('media-box', 'lazyload');
     img.dataset.sizes = 'auto';
+    img.dataset.lowsrc = blurImg;
+    img.dataset.src = download_url;
     img.width = width;
     img.height = height;
     img.alt = author;
-    mediabox.appendChild(img);
-    item.appendChild(mediabox);
+    item.appendChild(img);
+
     return item;
 };
 
-fetch('https://picsum.photos/v2/list?limit=30')
+const modalAnchorComponent = function modalAnchorComponent(
+    link = 'https://google.com'
+) {
+    const anchor = document.createElement('a');
+    anchor.target = '_blank';
+    anchor.rel = 'noreferrer';
+    anchor.href = link;
+
+    return anchor;
+};
+
+const modalComponent = function modalComponent(data) {
+    const {
+        name = 'Place name',
+        source = 'Source name',
+        source_link = 'https://google.com',
+        lat = 47,
+        lng = 25,
+        url = 'https://picsum.photos/400/800',
+    } = data;
+    const modalDiv = document.createElement('div');
+    modalDiv.classList.add('modal');
+
+    const baseDiv = document.createElement('div');
+    modalDiv.appendChild(baseDiv);
+
+    const img = document.createElement('img');
+    baseDiv.appendChild(img);
+    img.src = url;
+    img.alt = name;
+
+    const descDiv = document.createElement('div');
+    baseDiv.appendChild(descDiv);
+
+    const placeName = document.createElement('p');
+    descDiv.appendChild(placeName);
+    placeName.textContent = name;
+
+    const sourceLink = modalAnchorComponent(source_link);
+    descDiv.appendChild(sourceLink);
+
+    const sourceName = document.createElement('p');
+    sourceLink.appendChild(sourceName);
+    sourceName.textContent = source;
+
+    const mapLink = modalAnchorComponent(
+        `https://maps.google.com/?q=${lat},${lng}`
+    );
+    descDiv.appendChild(mapLink);
+
+    const button = document.createElement('button');
+    mapLink.appendChild(button);
+
+    const pinIcon = document.createElement('i');
+    button.appendChild(pinIcon);
+    pinIcon.classList.add('fas', 'fa-map-marker-alt');
+    button.append('Map');
+
+    return modalDiv;
+};
+
+fetch('https://picsum.photos/v2/list?limit=100')
     .then((res) => res.json())
     .then((res) => {
         res.forEach((item) => {
@@ -46,16 +98,28 @@ fetch('https://picsum.photos/v2/list?limit=30')
         });
     })
     .then(() => {
-        imagesLoaded(grid).on('progress', () => {
-            masonry.layout();
+        new Isotope(grid, {
+            itemSelector: '.grid-item',
+            percentPosition: true,
+            masonry: {
+                columnWidth: '.grid-sizer',
+            },
+        });
+
+        const gridItems = document.querySelectorAll('.grid-item');
+        gridItems.forEach((el) => {
+            const img = el.querySelector('img');
+            img.onclick = function onClick() {
+                modal = modalComponent({});
+                document.body.appendChild(modal);
+                modal.onclick = function onClick() {
+                    this.remove();
+                };
+            };
         });
     })
     .catch((err) => console.log(err));
 
-snapContainer.onscroll = function onScroll() {
-    masonry.layout();
-};
-
 scrollButton.onclick = function onClick() {
-    main.scrollIntoView({ behavior: 'smooth' });
+    grid.scrollIntoView({ behavior: 'smooth' });
 };
